95b87c74d92a49d66b083d376dcfc7e20a3cfbfb
madomaga <marcin.domagala@capgemini.com>
2023-12-18 17:00:07 +0100

    spinner test

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,11 @@
-
-
+'use client'
+import { DNA } from 'react-loader-spinner'
 export default function About() {
-    return <div><h1>About</h1></div>
-
-
+  return (
+    <div>
+      <h1>
+        <DNA />
+      </h1>
+    </div>
+  )
 }
